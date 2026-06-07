bf2e85c8bd63e9770b98aeed5a536bd43ff484fd
JuliusRock <pawdcast.uk@gmail.com>
2026-06-07 11:43:01 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@ function armLift() {
   // rotate(45deg)  → gira para direita
   if (armBottom) {
     armBottom.style.transition = 'transform 0.8s ease';
-    armBottom.style.transform = 'rotate(-25deg)'; // ← SENTIDO E ÂNGULO AQUI
+    armBottom.style.transform = 'rotate(-45deg)'; // ← SENTIDO E ÂNGULO AQUI
   }
   armOut = true;
 }
